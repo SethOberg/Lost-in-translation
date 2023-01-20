7e0f380f7f03259637a352ce64ccd1e7ab9c2c07
SethOberg <seob95@gmail.com>
2023-01-20 09:14:02 +0100

    Added separate css file for login screen

--- src/login/WelcomePage.js
+++ src/login/WelcomePage.js
@@ -1,11 +1,21 @@
 import React from "react";
 import WelcomeText from "./WelcomeText";
+import "./login.css";
 
 const WelcomePage = () => {
   return (
     <div>
       <WelcomeText />
       <div id="logInSection">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
+        </div>
         <button class="purpleBtn">Log in</button>
       </div>
     </div>
