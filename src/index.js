e01ac1a79c45c2a270bd24a238896e6a6820ef14
SethOberg <seob95@gmail.com>
2023-01-20 10:24:43 +0100

    Fixed UI on LoginScreen

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./shared/main.css";
 import TranslationHeader from "./shared/TranslationHeader";
 import WelcomePage from "./login/WelcomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
