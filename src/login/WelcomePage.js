ec36dbbd5260c5fbe5de3bfc9fdd74416134e68b
SethOberg <seob95@gmail.com>
2023-01-20 16:28:30 +0100

    Get and check input

--- src/login/WelcomePage.js
+++ src/login/WelcomePage.js
@@ -2,28 +2,66 @@ import React from "react";
 import WelcomeText from "./WelcomeText";
 import "./login.css";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
 import TranslationHeader from "../shared/TranslationHeader";
+import { useForm } from "react-hook-form";
+
+const userNameConfig = {
+  required: true,
+  minLength: 2,
+};
 
 const WelcomePage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  console.log(errors);
+
   return (
     <div>
       <TranslationHeader />
       <WelcomeText />
       <div id="logInSection">
         <div className="input-group mb-3" id="loginInput">
-          {/* <InputGroup className="mb-3 test"> */}
           <Form.Control
             placeholder="Enter username..."
             aria-label="Username"
             aria-describedby="usernameHelpBlock"
           />
-          <Form.Text id="usernameHelpBlock" muted>
-            Show text depending on username ok or already taken
+          <Form.Text id="usernameHelpBlock helpTxt" muted>
+            Username too short
           </Form.Text>
-          {/* </InputGroup> */}
         </div>
-        <button class="purpleBtn">Log in</button>
+        <button className="purpleBtn">Log in</button>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset>
+            <input
+              type="text"
+              placeholder="Enter username..."
+              {...register("username", userNameConfig)}
+              id="loginInput2"
+            />
+            <br />
+            <p id="inputHelpTxt">
+              {errors.username && errors.username.type === "required" && (
+                <span>Username is required</span>
+              )}
+              {errors.username && errors.username.type === "minLength" && (
+                <span>Too short</span>
+              )}
+            </p>
+          </fieldset>
+          <button type="submit" className="purpleBtn" id="signUpBtn">
+            Get started
+          </button>
+        </form>
       </div>
     </div>
   );
