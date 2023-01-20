00631dec9b3048a629e06f28ff7a65903584bd5b
SethOberg <seob95@gmail.com>
2023-01-20 15:30:16 +0100

    Added logo

--- src/login/WelcomeText.js
+++ src/login/WelcomeText.js
@@ -1,8 +1,10 @@
 import React from "react";
+import logo from "../images/Logo-Hello.png";
 
 const WelcomeText = () => {
   return (
     <article id="WelcomeText">
+      <img src={logo} alt="" width="150px" />
       <h1>Get started</h1>
     </article>
   );
